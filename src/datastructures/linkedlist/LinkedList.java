5bb8d7651b4a6b8079a5cce001ce9984b8acc965
Momcilo Mikovic <miiikovic19@gmail.com>
2023-04-30 13:38:55 +0200

    Added method from /Users/momcilomikovic/Desktop/Methods/LinkedListMethods

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -8,15 +8,31 @@ public class LinkedList {
 
 
 
-public boolean contains(E element) {
-    Node<E> current = head;
-    while (current != null) {
-        if (current.element.equals(element)) {
-            return true;
-        }
-        current = current.next;
+
+
+
+
+
+public void add(int index, E element) {
+    if (index < 0 || index > size) {
+        throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
     }
-    return false;
+    Node<E> newNode = new Node<>(element);
+    if (index == 0) {
+        newNode.next = head;
+        head = newNode;
+        if (tail == null) {
+            tail = head;
+        }
+    } else if (index == size) {
+        tail.next = newNode;
+        tail = newNode;
+    } else {
+        Node<E> prev = getNode(index - 1);
+        newNode.next = prev.next;
+        prev.next = newNode;
+    }
+    size++;
 }
 
 

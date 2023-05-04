365d82dbf81481258157951d9c139795c2c496c0
Momcilo Mikovic <miiikovic19@gmail.com>
2023-05-04 18:13:13 +0200

    Added method from /Users/momcilomikovic/Desktop/Methods/LinkedListMethods

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -3,21 +3,32 @@ package datastructures.linkedlist;
 public class LinkedList {
 
 	
-    public E get(int index) {
-        if (index < 0 || index >= size) {
+    
+
+
+
+
+    public void add(int index, E element) {
+        if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-        return getNode(index).element;
-    }
-
-    private Node<E> getNode(int index) {
-        Node<E> current = head;
-        for (int i = 0; i < index; i++) {
-            current = current.next;
+        Node<E> newNode = new Node<>(element);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+            if (tail == null) {
+                tail = head;
+            }
+        } else if (index == size) {
+            tail.next = newNode;
+            tail = newNode;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
         }
-        return current;
+        size++;
     }
 }
-
 
 
